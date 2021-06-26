bb90a5438faf2b7fea21f24b62eea41204ae851b
Henry Cook <hjfcook@hotmail.co.uk>
2021-06-26 19:19:18 +0100

    continued developing coffee page with products

--- src/Pages/Coffee/Coffee.js
+++ src/Pages/Coffee/Coffee.js
@@ -2,62 +2,28 @@ import styles from './Coffee.module.css'
 import Header from '../../Components/Header/Header.js'
 
 function CoffeePage() {
-    return (
-        <div>
-            <Header />
-            <div className={styles.coffeeSelection}>
-                <div className={styles.coffeeSlot}>
-                    coffee 1
-                </div>
-                <div className={styles.coffeeSlot}>
-                    coffee 2
-                </div>
-                <div className={styles.coffeeSlot}>
-                    coffee 3
-                </div>
-                <div className={styles.coffeeSlot}>
-                    coffee 4
-                </div>
-                <div className={styles.coffeeSlot}>
-                    coffee 5
-                </div>
-                <div className={styles.coffeeSlot}>
-                    coffee 6
-                </div>
-                <div className={styles.coffeeSlot}>
-                    coffee 7
-                </div>
-                <div className={styles.coffeeSlot}>
-                    coffee 8
-                </div>
-                <div className={styles.coffeeSlot}>
-                    coffee 9
-                </div>
-                <div className={styles.coffeeSlot}>
-                    coffee 10
-                </div>
-                <div className={styles.coffeeSlot}>
-                    coffee 11
-                </div>
-                <div className={styles.coffeeSlot}>
-                    coffee 12
-                </div>
-                <div className={styles.coffeeSlot}>
-                    coffee 13
-                </div>
-                <div className={styles.coffeeSlot}>
-                    coffee 14
-                </div>
-                <div className={styles.coffeeSlot}>
-                    coffee 15
-                </div>
-                <div className={styles.coffeeSlot}>
-                    coffee 16
-                </div>
-            </div>
-            <div>footer</div>
+  const coffeeList = [];
+  for (let i = 0; i < 15; i++) {
+    coffeeList.push(
+      <div className={styles.coffeeSlot}>
+        <div className={styles.coffeeInfo}>
+          <div className={styles.coffeeName}>Coffee {i+1}</div>
+          <div>Ethiopia</div>
+          <div>£14</div>
         </div>
+      </div>
     );
+  }
+
+  return (
+    <div>
+      <Header />
+      <div className={styles.coffeeSelection}>
+        {coffeeList}
+      </div>
+      <div>footer</div>
+    </div>
+  );
 }
 
 export default CoffeePage;
