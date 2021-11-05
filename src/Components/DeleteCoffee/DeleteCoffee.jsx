59848c15a60836cbb2e3e8371ec50ecb975918ad
Henry Cook <hjfcook@hotmail.co.uk>
2021-11-05 15:10:18 +0000

    full CRUD basic

---
+++ src/Components/DeleteCoffee/DeleteCoffee.jsx
@@ -0,0 +1,39 @@
+import styles from './DeleteCoffee.module.css';
+
+import {useState, useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
+
+function AddCoffee(props) {
+  const history = useHistory();
+
+  const deleteCoffee = () => {
+    fetch(`http://localhost:3000/api/coffee/${props.coffee._id}`, {
+      method: "DELETE",
+      headers: {'Content-Type': 'application/json'},
+      // body: JSON.stringify({
+      // }),
+      credentials: 'include'
+    }).then((res) => res.json())
+      .then(res => {
+        console.log(res);
+        if (res.deleted) {
+          history.push('/admin');
+          props.fetchData();
+        }
+      });
+  };
+
+  return (
+    <>
+      {/* <h1>add coffee</h1> */}
+      <h2>are you sure you want to delete this coffee?</h2>
+      <h3>this action CANNOT be undone</h3>
+      <div className={styles.buttonDiv}>
+        <button onClick={() => history.push('/admin/edit/' + props.coffee.name.toLowerCase().replace(/ /g, '-'))}>cancel</button>
+        <button onClick={deleteCoffee} className={styles.dangerButton}>delete</button>
+      </div>
+    </>
+  );
+}
+
+export default AddCoffee;
--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -1,24 +1,31 @@
 
 import styles from './Admin.module.css';
-import Header from '../../Components/Header/Header.jsx';
-import Footer from '../../Components/Footer/Footer.jsx';
+import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
+import AddCoffee from '../../Components/AddCoffee/AddCoffee';
+import EditCoffee from '../../Components/EditCoffee/EditCoffee';
+import DeleteCoffee from '../../Components/DeleteCoffee/DeleteCoffee';
 
 import {useState, useEffect} from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import {Switch, Route, useHistory} from 'react-router-dom';
 
 function Admin() {
   const [coffeeProducts, setCoffeeProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [sorting, setSorting] = useState({field: null, ascending: false});
+  
+  const history = useHistory();
+
+  const fetchData = async() => {
+    setIsLoading(true);
+    const dataRead = await (await fetch('http://localhost:3000/api/coffee')).json();
+    setCoffeeProducts(dataRead);
+    setIsLoading(false);
+  };
 
   useEffect(() => {
-    const fetchData = async() => {
-      setIsLoading(true);
-      const dataRead = await (await fetch('http://localhost:3000/api/coffee')).json();
-      setCoffeeProducts(dataRead);
-      setIsLoading(false);
-    };
     fetchData();
   }, []);
 
@@ -66,49 +73,68 @@ function Admin() {
     <div className={styles.adminPage}>
       <Header />
       <div className={styles.adminBlock}>
-        <h1>admin</h1>
-        {/* <p>{JSON.stringify(coffeeProducts)}</p> */}
-        {isLoading ?
-          <p>loading...</p>
-          :
-          <table>
-            <thead>
-              <tr>
-                <th onClick={() => {updateSortingField('name')}}>Name {selectSymbol('name')}</th>
-                <th onClick={() => {updateSortingField('country')}}>Country {selectSymbol('country')}</th>
-                <th onClick={() => {updateSortingField('process')}}>Process {selectSymbol('process')}</th>
-                <th onClick={() => {updateSortingField('price')}}>Price {selectSymbol('price')}</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {coffeeProducts.map(product => (
-                <tr key={product._id}>
-                  <td>
-                    {product.name}
-                  </td>
-                  <td>
-                    {product.country}
-                  </td>
-                  <td>
-                    {product.process}
-                  </td>
-                  <td>
-                    {`£${((product.price*100)/100).toFixed(2)}`}
-                  </td>
-                  <td>
-                    <FontAwesomeIcon
-                      icon={faEdit}
-                    />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        }
-        <div className={styles.buttonDiv}>
-          <button>add coffee</button>
-        </div>
+        <Switch>
+          <Route exact path='/admin'>
+            <h1>admin</h1>
+            {/* <p>{JSON.stringify(coffeeProducts)}</p> */}
+            {isLoading ?
+              <p>loading...</p>
+              :
+              <table>
+                <thead>
+                  <tr>
+                    <th onClick={() => {updateSortingField('name')}}>Name {selectSymbol('name')}</th>
+                    <th onClick={() => {updateSortingField('country')}}>Country {selectSymbol('country')}</th>
+                    <th onClick={() => {updateSortingField('process')}}>Process {selectSymbol('process')}</th>
+                    <th onClick={() => {updateSortingField('price')}}>Price {selectSymbol('price')}</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {coffeeProducts.map(product => (
+                    <tr key={product._id}>
+                      <td>
+                        {product.name}
+                      </td>
+                      <td>
+                        {product.country}
+                      </td>
+                      <td>
+                        {product.process}
+                      </td>
+                      <td>
+                        {`£${((product.price*100)/100).toFixed(2)}`}
+                      </td>
+                      <td>
+                        <FontAwesomeIcon
+                          icon={faEdit}
+                          className={styles.icon}
+                          onClick={() => {history.push('/admin/edit/' + product.name.toLowerCase().replace(/ /g, '-'))}}
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            }
+            <div className={styles.buttonDiv}>
+              <button onClick={() => {history.push('/admin/add')}} className={styles.primaryButton}>add coffee</button>
+            </div>
+          </Route>
+          <Route path='/admin/add'>
+            <AddCoffee fetchData={fetchData}/>
+          </Route>
+          {coffeeProducts.map(product => (
+            <Route path={'/admin/edit/' + product.name.toLowerCase().replace(/ /g, '-')}>
+              <EditCoffee coffee={product} fetchData={fetchData}/>
+            </Route>
+          ))}
+          {coffeeProducts.map(product => (
+            <Route path={'/admin/delete/' + product._id}>
+              <DeleteCoffee coffee={product} fetchData={fetchData}/>
+            </Route>
+          ))}
+        </Switch>
       </div>
       <Footer />
     </div>
